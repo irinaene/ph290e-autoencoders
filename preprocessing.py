352de3e27f0e3cdbd045aafc36eaca03580aae17
Irina Ene <irina.ene@cern.ch>
2022-04-12 20:17:51 -0700

    add preprocessing scripts

---
+++ preprocessing.py
@@ -0,0 +1,104 @@
+import ROOT
+import sys
+import os
+import numpy as np
+import h5py
+
+import preprocessing_helpers as pph
+
+ROOT.gROOT.SetBatch(True)
+# Load Delphes
+ROOT.gSystem.Load('libDelphes.so')
+
+# Check for input arguments
+if len(sys.argv)==1:
+    print('usage: {} input0.root [input1.root ...]')
+    sys.exit(1)
+inputs=sys.argv[1:]
+
+select_ttbar = False
+if "ttbar" in inputs[0]:
+    select_ttbar = True
+
+# Load input files
+t=ROOT.TChain('Delphes')
+for inpath in inputs:
+    t.AddFile(inpath)
+
+if select_ttbar:
+    nJets = 2104
+else:
+    nJets = 233178
+
+tau_2_arr = np.empty(nJets)
+tau_3_arr = np.empty(nJets)
+jet_images_orig = np.empty((nJets, 50, 50))
+jet_images_preproc = np.empty((nJets, 50, 50))
+
+# Loop the loop
+nJets = 0
+nEvents = 0
+for e in t:
+    nEvents += 1
+    if nEvents % 1000 == 0:
+        print(f"Processing events: {nEvents}")
+    
+    for fj in e.FatJet:
+        # Simple selection
+        if fj.PT < 500: continue
+        if select_ttbar:
+            if abs(fj.Mass - 173) > 20: continue
+        
+        # tau_2, tau_3 variables for baseline performance
+        tau_2_arr[nJets] = fj.Tau[1]
+        tau_3_arr[nJets] = fj.Tau[2]
+        
+        # for storing the jet images
+        eta_arr = []
+        phi_arr = []
+        et_arr = []
+
+        # Add constituents to the image
+        nConstituents = 0
+        for c in fj.Constituents:
+            if type(c) is not ROOT.Tower:
+                continue # not a calorimeter thing
+            nConstituents += 1
+            eta_arr.append(c.Eta)
+            phi_arr.append(c.Phi)
+            et_arr.append(c.ET)
+        
+        if nConstituents == 0: break
+        
+        eta_arr = np.array(eta_arr)
+        phi_arr = np.array(phi_arr)
+        et_arr = np.array(et_arr)
+        
+        # original jet images -- only centered and pixelated
+        eta_cent, phi_cent = pph.center(eta_arr, phi_arr, et_arr)
+        jet_img = pph.pixelize(eta_cent, phi_cent, et_arr)
+        jet_images_orig[nJets] = jet_img
+        
+        # fully pre-processed jet images
+        eta_rot, phi_rot = pph.rotate_princ_vertical(eta_cent, phi_cent, et_arr)
+        flip = pph.determine_flip(eta_rot, phi_rot, et_arr)
+        eta_flip, phi_flip = pph.flip(eta_rot, phi_rot, flip)
+        jet_img_proc = pph.pixelize(eta_flip, phi_flip, et_arr)
+        jet_images_preproc[nJets] = jet_img_proc
+        
+        nJets += 1
+        break # Leading jet only
+
+# now save the data to hdf5 format
+outDir = "preprocessed_samples"
+if not os.path.isdir(outDir):
+    os.mkdir(outDir)
+outFile = "signal.hdf5" if select_ttbar else "background.hdf5"
+outPath = os.path.join(outDir, outFile)
+
+f1 = h5py.File(outPath, "w")
+dset1 = f1.create_dataset("tau_2", tau_2_arr.shape, dtype="f", data=tau_2_arr)
+dset2 = f1.create_dataset("tau_3", tau_3_arr.shape, dtype="f", data=tau_3_arr)
+dset3 = f1.create_dataset("jet_images_original", jet_images_orig.shape, dtype="f", data=jet_images_orig)
+dset4 = f1.create_dataset("jet_images_preprocessed", jet_images_preproc.shape, dtype="f", data=jet_images_preproc)
+f1.close()
---
+++ preprocessing_helpers.py
@@ -0,0 +1,78 @@
+"""Preprocessing helper functions."""
+
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import sys
+
+def center(x, y, w):
+    centroid_x = np.average(x, weights=w)
+    centroid_y = np.average(y, weights=w)
+    
+    x = x - centroid_x
+    y = y - centroid_y
+    
+    return x, y
+
+def rotate_princ_vertical(x, y, weights):
+    if len(x) == 1:
+        theta = np.arctan2(y[0], x[0])
+    else:
+        # compute PCA from covariance matrix
+        x_y_mat = np.array([x, y])
+        # weighted covariance
+        wcov = np.cov(x_y_mat, aweights=weights)
+        # eigenvalues and eigenvectors
+        w, v = np.linalg.eig(wcov)
+        # highest eigenval
+        princ = v[:, np.argmax(w)]
+        theta = np.arctan2(princ[1], princ[0])
+    
+    theta_rot = -theta + np.pi /2 
+    x_rot = x * np.cos(theta_rot) - y * np.sin(theta_rot)
+    y_rot = x * np.sin(theta_rot) + y * np.cos(theta_rot)
+    
+    return x_rot, y_rot
+
+def flip(x, y, flip):
+    if flip not in ["LR", "UD", "BOTH", "NONE"]:
+        # panic mode
+        print(f"Flip type not defined: {flip}")
+        sys.exit(1)
+    if flip == "LR":
+        x *= -1
+    elif flip == "UD":
+        y *= -1
+    elif flip == "BOTH":
+        x *= -1
+        y *= -1
+    
+    return x, y
+
+def determine_flip(x, y, w):
+    # find the index of the maximum intensity
+    ind_max = np.argmax(w)
+    x_max = x[ind_max]
+    y_max = y[ind_max]
+    
+    flip_types = []
+    if x_max < 0:
+        flip_types.append("LR")
+    if y_max < 0:
+        flip_types.append("UD")
+    
+    if len(flip_types) == 0:
+        return "NONE"
+    elif len(flip_types) == 2:
+        return "BOTH"
+    return flip_types[0]
+
+def normalize(x):
+    return x / np.sum(x)
+
+def pixelize(x, y, vals):
+    nPix = 50
+    pix_bins = np.linspace(-1, 1, nPix + 1)
+    vals_pix, _, _ = np.histogram2d(x, y, bins=pix_bins, weights=vals)
+    
+    return vals_pix
