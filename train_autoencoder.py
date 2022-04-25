f2962e205949710899b131205c80536942d0bc5f
Irina Ene <irina.ene@cern.ch>
2022-04-24 18:06:10 -0700

    update training script

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -1,38 +1,41 @@
 import h5py
 import os
 import tensorflow as tf
+import numpy as np
 import training_helpers as trh
 import plotting_helpers as ploth
 
 nPix = 40
 sampleDir = "training_samples"
 batch_size = 1024
-validation_split = 0.2
-nEpochs = 100
+nEpochs = 2000
 patience = 3
-encoding_dim = 6
-outDir = "trained_models"
+encoding_dim = 12
+outDir = f"trained_models_dim{encoding_dim}"
+rng_seed = 12345
+
+# create output folders
+if not os.path.exists(outDir):
+    os.mkdir(outDir)
 
 # load the training samples
 f_train = h5py.File(os.path.join(sampleDir, "background_train.hdf5"), "r")
-bkg_train = f_train["jet_images_proc_normalized"][:]
-assert bkg_train.shape[1] == nPix
-assert bkg_train.shape[2] == nPix
+bkg_sample = f_train["jet_images_proc_normalized"][:]
+assert bkg_sample.shape[1] == nPix
+assert bkg_sample.shape[2] == nPix
 f_train.close()
 
-# create the autoencoder model
-autoencoder_model = trh.create_autoencoder_model((nPix, nPix), encoding_dim=encoding_dim)
-autoencoder_model.summary()
+# shuffle the jets (again)
+rng = np.random.default_rng(rng_seed)
+inds = rng.choice(np.arange(bkg_sample.shape[0]), size=125000, replace=False)
 
-# train the autoencoder
-callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=patience)
-history = autoencoder_model.fit(x=bkg_train, y=bkg_train, validation_split=validation_split,
-        epochs=nEpochs, batch_size=batch_size, callbacks=[callback])
+# 100k jets for training, 25k jets for validation
+bkg_train = bkg_sample[inds[:100000]]
+bkg_valid = bkg_sample[inds[100000:]]
 
-# save model to hdf5 file
-if not os.path.exists(outDir):
-    os.mkdir(outDir)
-autoencoder_model.save(os.path.join(outDir, "trained_autoencoder.h5"))
+# create and train autoencoder model
+tf.keras.backend.clear_session()
+history = trh.train_autoencoder_model(bkg_train, bkg_valid, nPix, encoding_dim, nEpochs, batch_size, outDir, patience=patience)
 
 # training validation plots
-ploth.plot_training_loss(history)
+ploth.plot_training_loss(history, outDir=outDir)
