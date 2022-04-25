30ff5abb3a397e512f74523f7811e35b81161413
Irina Ene <irina.ene@cern.ch>
2022-04-24 18:04:02 -0700

    add new training helper functions

--- training_helpers.py
+++ training_helpers.py
@@ -1,9 +1,12 @@
 """Helper functions for the training set."""
 
+import os
+
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Reshape, UpSampling2D, Activation
 from tensorflow.keras import Model
 from tensorflow.keras.losses import mean_squared_error
 from tensorflow.keras.optimizers import Adam
+from tensorflow import keras
 
 
 def create_autoencoder_model(img_shape, encoding_dim=6):
@@ -36,3 +39,54 @@ def create_autoencoder_model(img_shape, encoding_dim=6):
     autoencoder.compile(loss=mean_squared_error , optimizer=Adam())
     
     return autoencoder
+
+def train_encoding_dim_search(train_data, nPix, encoding_dim, nEpochs, batch_size, trial, logDir, patience=3):
+    print(f"\nRunning training for encoding dim {encoding_dim} -- iteration {trial + 1}")
+    # create the autoencoder model
+    autoencoder_model = create_autoencoder_model((nPix, nPix), encoding_dim=encoding_dim)
+
+    # callbacks
+    early_stopping_callback = EarlyStopping(monitor="loss", patience=patience, verbose=1)
+    chkPath = os.path.join(logDir, f"checkpoint_file_iter{trial + 1}.h5")
+    model_checkpt_callback = ModelCheckpoint(chkPath, monitor="loss", mode="min",
+            save_best_only=True)
+    logPath = os.path.join(logDir, f"training_iter{trial + 1}.log")
+    csv_logger = CSVLogger(logPath)
+
+    # train the autoencoder
+    history = autoencoder_model.fit(x=train_data, y=train_data, epochs=nEpochs, batch_size=batch_size,
+            callbacks=[early_stopping_callback, model_checkpt_callback, csv_logger])
+
+    return history
+
+def train_autoencoder_model(train_data, valid_data, nPix, encoding_dim, nEpochs, batch_size, logDir, trial=0, patience=3):
+    print(f"\nRunning training for encoding dim {encoding_dim} -- iteration {trial + 1}")
+    # create the autoencoder model
+    autoencoder_model = create_autoencoder_model((nPix, nPix), encoding_dim=encoding_dim)
+
+    # callbacks
+    early_stopping_callback = EarlyStopping(monitor="val_loss", patience=patience, verbose=1)
+    chkPath = os.path.join(logDir, f"checkpoint_file_iter{trial + 1}.h5")
+    model_checkpt_callback = ModelCheckpoint(chkPath, monitor="val_loss", mode="min",
+            save_best_only=True)
+    logPath = os.path.join(logDir, f"training_iter{trial + 1}.log")
+    csv_logger = CSVLogger(logPath)
+
+    # train the autoencoder
+    history = autoencoder_model.fit(x=train_data, y=train_data, validation_data=(valid_data, valid_data),
+            epochs=nEpochs, batch_size=batch_size,
+            callbacks=[early_stopping_callback, model_checkpt_callback, csv_logger])
+
+    return history
+
+def load_autoencoder_model(modelPath):
+    autoencoder = keras.models.load_model(modelPath)
+
+    return autoencoder
+
+def evaluate_model_on_set(autoencoder, input_imgs, batch_size=1024):
+    output_imgs = autoencoder.predict(input_imgs, batch_size=batch_size, verbose=1)
+    # reshape output from model to have the same shape as the input
+    output_imgs = output_imgs[:, :, :, 0]
+     
+    return output_imgs
