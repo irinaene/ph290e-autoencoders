7679981975f8ae9004e3414f9f9d4d3cc6f81b7d
Irina Ene <irina.ene@cern.ch>
2022-04-24 18:03:33 -0700

    add new evaluation helper functions

--- evaluation_helpers.py
+++ evaluation_helpers.py
@@ -14,6 +14,31 @@ def roc_curve(sig_arr, bkg_arr, thresholds, cut_type="greater"):
         bkg_eff = np.cumsum(bkg_hist[::-1]) / n_bkg
     elif cut_type == "lower":
         sig_eff = np.cumsum(sig_hist) / n_sig
-        bkg_eff = np.cumsum(bkg_hist) / n_sig
+        bkg_eff = np.cumsum(bkg_hist) / n_bkg
     
     return sig_eff, bkg_eff
+
+def compute_reconstruction_error(input_img, output_img):
+    diff_img = input_img - output_img
+    sqr_diff = np.square(diff_img)
+    mean_reco_err = np.average(sqr_diff, axis=(1, 2))
+    return mean_reco_err
+
+def equalObs(x, nbin):
+    # define function to calculate equal-frequency bins
+    # from https://www.statology.org/equal-frequency-binning-python/
+    nlen = len(x)
+    return np.interp(np.linspace(0, nlen, nbin + 1),
+                     np.arange(nlen),
+                     np.sort(x))
+
+def jet_mass_vs_reco_error(reco_err, jet_mass, bins=25):
+    counts, bin_edges = np.histogram(reco_err, bins=bins)
+    mass, bin_edges = np.histogram(reco_err, weights=jet_mass, bins=bin_edges)
+
+    print(counts)
+
+    avg_jet_mass = mass / counts
+    bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2
+    
+    return avg_jet_mass, bin_centers
